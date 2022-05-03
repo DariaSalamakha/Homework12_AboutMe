8a488fa6d544e17b221be73dfd67e275a90b24c2
Daria Salamakha <daria.salamakha@gmail.com>
2022-05-03 12:43:44 +0300

    Upload full project.

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Daria Salamakha on 06.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
